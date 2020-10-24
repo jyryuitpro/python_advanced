9cffaee1078a1e03eb1a098b211f670917b30e44
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-24 18:17:26 +0900

    01. Class & Method - 01. Class Advanced(1) - 1

---
+++ chapter01_01.py
@@ -0,0 +1,37 @@
+# Chapter01-1
+# 파이썬 심화
+# 객체 지향 프로그래밍(OOP) -> 코드의 재사용, 코드 중복 방지 등
+# 클래스 상세 설명
+# 클래스 변수, 인스턴스 변수
+
+# 일반적인 코딩
+
+# 학생1
+student_name_1 = 'Kim'
+student_number_1 = 1
+student_grade_1 = 1
+student_detail_1 = [
+    {'gender': 'Male'},
+    {'score1': 95},
+    {'score2': 88}
+]
+
+# 학생2
+student_name_2 = 'Lee'
+student_number_2 = 2
+student_grade_2 = 2
+student_detail_2 = [
+    {'gender': 'Female'},
+    {'score1': 77},
+    {'score2': 92}
+]
+
+# 학생3
+student_name_3 = 'Park'
+student_number_3 = 3
+student_grade_3 = 4
+student_detail_3 = [
+    {'gender': 'Male'},
+    {'score1': 99},
+    {'score2': 100}
+]
