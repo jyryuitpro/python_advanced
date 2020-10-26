5e67f11ad4bfd3e733d115ce7d34122aec3c1285
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-26 23:56:40 +0900

    02. Class & Method - 02. Class Advanced(1) - 2

--- chapter01_01.py
+++ chapter01_01.py
@@ -35,3 +35,42 @@ student_detail_3 = [
     {'score1': 99},
     {'score2': 100}
 ]
+
+# 리스트 구조
+# 관리하기 불편
+# 데이터의 정확한 위치(인덱스)매핑 해서 사용
+student_names_list = ['Kim', 'Lee', 'Park']
+student_number_list = [1, 2, 3]
+student_grade_list = [1, 2, 4]
+student_detail_list = [{'gender': 'Male', 'score1': 95, 'score2': 92}, {'gender': 'Female', 'score1': 77, 'score2': 88}, {'gender': 'Male', 'score1': 99, 'score2': 100}]
+
+# 학생 삭제
+del student_names_list[1]
+del student_number_list[1]
+del student_grade_list[1]
+del student_detail_list[1]
+
+print(student_names_list)
+print(student_number_list)
+print(student_grade_list)
+print(student_detail_list)
+
+print()
+print()
+
+# 딕셔너리 구조
+# 코드 반복 지속, 중첩 문제
+student_dicts = [
+    {'student_names': 'Kim', 'student_number': 1, 'student_grade': 1, 'student_detail': {'gender': 'Male', 'score1': 95, 'score2': 92}},
+    {'student_names': 'Lee', 'student_number': 2, 'student_grade': 2, 'student_detail': {'gender': 'Female', 'score1': 77, 'score2': 88}},
+    {'student_names': 'Park', 'student_number': 3, 'student_grade': 4, 'student_detail': {'gender': 'Male', 'score1': 99, 'score2': 100}}
+]
+
+print(student_dicts)
+del student_dicts[1]
+
+print()
+print()
+
+print(student_dicts)
+
