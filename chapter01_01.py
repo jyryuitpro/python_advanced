e997b996c4b6ffc2dfddb8aba93097cf0bd61fcd
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-28 00:12:58 +0900

    03. Class & Method - 03. Class Advanced(1) - 3

--- chapter01_01.py
+++ chapter01_01.py
@@ -74,3 +74,29 @@ print()
 
 print(student_dicts)
 
+print()
+print()
+
+# 클래스 구조
+# 구조 설계 후 재사용성 증가, 코드 반복 최소화, 메소드 활용
+
+class Student():
+    def __init__(self, name, number, grade, details):
+        self._name = name
+        self._number = number
+        self._grade = grade
+        self._details = details
+
+    def __str__(self):
+        return 'str : {}'.format(self._name)
+
+student1 = Student('Kim', 1, 1, {'gender': 'Male', 'score1': 95, 'score2': 92})
+student2 = Student('Lee', 2, 2, {'gender': 'Female', 'score1': 77, 'score2': 92})
+student3 = Student('Park', 3, 4, {'gender': 'Male', 'score1': 99, 'score2': 10})
+
+print(student1.__dict__)
+print(student2.__dict__)
+print(student3.__dict__)
+
+
+
