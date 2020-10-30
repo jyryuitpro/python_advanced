71b07008aee5603a94c662d626deb0cac3450087
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-30 09:59:00 +0900

    05. Class & Method - 05. Class Advanced(2) - 1

---
+++ chapter01_02.py
@@ -0,0 +1,39 @@
+# Chapter01-2
+# 파이썬 심화
+# 객체 지향 프로그래밍(OOP) -> 코드의 재사용, 코드 중복 방지 등
+# 클래스 상세 설명
+# 클래스 변수, 인스턴스 변수
+
+# 클래스 재 선언
+class Student():
+    """
+    Student Class
+    Author : jy.ryu.itpro
+    Date : 2020.10.30
+    """
+
+    # 클래스 변수
+    student_count = 0
+
+    def __init__(self, name, number, grade, details, email=None):
+        # 인스턴스 변수
+        self._name = name
+        self._number = number
+        self._grade = grade
+        self._details = details
+        self._email = email
+
+        Student.student_count += 1
+
+    def __str__(self):
+        return 'str {}'.format(self._name)
+
+    def __repr__(self):
+        return 'repr {}'.format(self._name)
+
+    def detail_info(self):
+        print('Current Id : {}'.format(id(self)))
+        print('Student Detail Info : {} {} {}'.format(self._name, self._email, self._details))
+
+    def __del__(self):
+        Student.student_count -= 1
