ccbe30503c3992982cebfe5ab668a732477a1eff
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-07 22:14:07 +0900

    08. Class & Method - 08. Class, Instance, Static Method - 1

--- chapter01_03.py
+++ chapter01_03.py
@@ -13,7 +13,7 @@ class Student(object):
     '''
 
     # Class Variable
-    tuition = 1.0
+    tuition_per = 1.0
 
     def __init__(self, id, first_name, last_name, email, grade, tuition, gpa):
         self._id = id
@@ -38,8 +38,50 @@ class Student(object):
 
     # Instance Method
     def get_fee_culc(self):
-        return 'After Tuition -> Id : {}, fee : {}'.format(self._id, self._tuition * Student.tuition)
+        return 'After Tuition -> Id : {}, fee : {}'.format(self._id, self._tuition * Student.tuition_per)
 
+    # Instance Method
+    def __str__(self):
+        return 'Student Info -> name: {} grade: {} email: {}'.format(self.full_name(), self._grade, self._email)
+
+    # Class Method : Student == cls
+    @classmethod
+    def raise_fee(cls, per):
+        if per <= 1:
+            print('Please Enter 1 or More')
+            return
+        cls.tuition_per = per
+        print('Succed! tuition increased!')
+
+
+
+student_1 = Student(1, 'Kim', 'Sarang', 'student1@naver.com', '1', 400, 3.5)
+student_2 = Student(2, 'Lee', 'Myungho', 'student2@daum.net', '2', 500, 4.3)
+
+# 기본정보
+print(student_1.__dict__)
+print(student_1)
+print(student_2)
+
+print()
+
+# 전체정보
+print(student_1.detail_info())
+print(student_2.detail_info())
+
+# 학비정보(인상전)
+print(student_1.get_fee())
+print(student_2.get_fee())
+
+# 학비인상(클래스 메소드 미사용)
+# Student.tuition_per = 1.2
+
+# 학비인상(클래스 메소드 사용)
+Student.raise_fee(1.5)
+
+# 학비정보(인상후)
+print(student_1.get_fee_culc())
+print(student_2.get_fee_culc())
 
 
 
